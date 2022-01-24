42d560d4290e9c1b9d99e9b82bbd877da4990042
osamamishmish <osama.mishmish16@gmail.com>
2022-01-24 06:49:12 +0200

    update the score

--- js/game.js
+++ js/game.js
@@ -1,43 +1,43 @@
 var fieldContent=[
     {
-       // name:1,
+       
         value:"X",
     },
     {
-       // name:2,
+       
         value:"O",
     },
     {
-       // name:3,
+       
         value:"X",
     },
     {
-       // name:4,
+       
         value:"O",
     },
     {
-      //  name:5,
+      
         value:"X",
     },
     {
-      //  name:6,
+      
         value:"O",
     },
     {
-       // name:7,
+       
         value:"X",
     },
     {
-      //  name:8,
+
         value:"O",
     },
     {
-      //  name:9,
+      
         value:"X",
     },
 ]
-var theFirstRow=document.querySelector(".first-row"),
-    theSecondRow=document.querySelector(".second-row");
+
+//fill the fields
 let index=0;
 $(document).ready(function(){
     $(".field").on("focus",function(){
@@ -48,52 +48,107 @@ $(document).ready(function(){
         
         index++;
         
+        
     })
 
 })
 
+
+
+
+
+
+//Player X Chances
 $(document).ready(function(){
     $(function(){
     $(".field").on("click",function(){
         for(let i=0 ;i<fieldContent.length;i++){
 
         if($(".field").eq(i-2).val()=="X"&&$(".field").eq(i-1).val()=="X"&&$(".field").eq(i).val()=="X"){
-            alert("Player X wins");
+            $(".referee").show().text("Player X wins");
         }
-        if($(".field").eq(i-2).val()=="O"&&$(".field").eq(i-1).val()=="O"&&$(".field").eq(i).val()=="O"){
-            alert("Player O wins");
-        }
+        
         if($(".first-row").children().eq(0).val()=="X"&&$(".second-row").children().eq(0).val()=="X"&&$(".third-row").children().eq(0).val()=="X"){
-            alert("PlayerX wins");
+            $(".referee").show().text("PlayerX wins");
         }
-        if($(".first-row").children().eq(0).val()=="O"&&$(".second-row").children().eq(0).val()=="O"&&$(".third-row").children().eq(0).val()=="O"){
-            alert("Player O wins");
-        }
-        if($(".first-row").children().eq(1).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(1).val()=="O"){
-            alert("Player O wins");
-        }
+        
         if($(".first-row").children().eq(1).val()=="X"&&$(".second-row").children().eq(1).val()=="X"&&$(".third-row").children().eq(1).val()=="X"){
-            alert("Player X wins");
+            $(".referee").show().text("Player X wins");
         }
         if($(".first-row").children().eq(2).val()=="X"&&$(".second-row").children().eq(2).val()=="X"&&$(".third-row").children().eq(2).val()=="X"){
-            alert("Player X wins");
+            $(".referee").show().text("Player X wins");
         }
-        if($(".first-row").children().eq(2).val()=="O"&&$(".second-row").children().eq(2).val()=="O"&&$(".third-row").children().eq(2).val()=="O"){
-            alert("Player O wins");
-        }
-        if($(".first-row").children().eq(0).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(2).val()=="O"){
-            alert("Player O wins");
-        }
+        
         if($(".first-row").children().eq(0).val()=="X"&&$(".second-row").children().eq(1).val()=="X"&&$(".third-row").children().eq(2).val()=="X"){
-            alert("Player X wins");
+            $(".referee").show().text("Player X wins");
         }
         if($(".first-row").children().eq(2).val()=="X"&&$(".second-row").children().eq(1).val()=="X"&&$(".third-row").children().eq(0).val()=="X"){
-            alert("Player X wins");
+            $(".referee").show().text("Player X wins");
         }
+       
+    }
+    })
+    })
+})
+
+
+// Player O Chances
+$(document).ready(function(){
+    $(function(){
+    $(".field").on("click",function(){
+        for(let i=0 ;i<fieldContent.length;i++){
+
+        
+        if($(".field").eq(i-2).val()=="O"&&$(".field").eq(i-1).val()=="O"&&$(".field").eq(i).val()=="O"){
+            $(".referee").show().text("Player O wins");
+        }
+       
+        if($(".first-row").children().eq(0).val()=="O"&&$(".second-row").children().eq(0).val()=="O"&&$(".third-row").children().eq(0).val()=="O"){
+            $(".referee").show().text("Player O wins");
+        }
+        if($(".first-row").children().eq(1).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(1).val()=="O"){
+            $(".referee").show().text("Player O wins");
+        }
+        
+        if($(".first-row").children().eq(2).val()=="O"&&$(".second-row").children().eq(2).val()=="O"&&$(".third-row").children().eq(2).val()=="O"){
+            $(".referee").show().text("Player O wins");
+        }
+        if($(".first-row").children().eq(0).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(2).val()=="O"){
+            $(".referee").show().text("Player O wins");
+        }
+        
         if($(".first-row").children().eq(2).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(0).val()=="O"){
-            alert("Player O wins");
+            $(".referee").show().text("Player O wins");
         }
     }
     })
+    })
+})
+
+
+
+//calculate the score
+let scoreX=0;
+let scoreO=0;
+$(".result-O").text(scoreO);
+$(".result-X").text(scoreX);
+$(document).ready(function(){
+    $(function(){
+        $(".button").on("click",function(){
+            $("input[type='text']").val("").attr("disabled",false);
+            $(".referee").hide();
+            index=0;
+                if($(".referee").text()=="Player O wins"){
+                    scoreO++;
+                return    $(".result-O").text(scoreO);
+                }
+                if($(".referee").text()=="Player X wins"){
+                    scoreX++;
+                  return  $(".result-X").text(scoreX);
+                }
+                
+             
+        })
+        
     })
 })
