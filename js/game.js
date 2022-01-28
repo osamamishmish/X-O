ccabdc1658b92242521dee068419cf52bf55ec64
osamamishmish <osama.mishmish16@gmail.com>
2022-01-28 15:33:18 +0200

    Update the score

--- js/game.js
+++ js/game.js
@@ -37,6 +37,11 @@ var fieldContent=[
     },
 ]
 
+let scoreX=0;
+let scoreO=0;
+$(".result-O").text(scoreO);
+$(".result-X").text(scoreX);
+
 //fill the fields
 let index=0;
 $(document).ready(function(){
@@ -65,19 +70,30 @@ $(document).ready(function(){
         for(let i=0 ;i<fieldContent.length;i++){
             if($(".field").eq(i).val()=="X"&&$(".field").eq(i).siblings().eq(0).val()=="X"&&$(".field").eq(i).siblings().eq(1).val()=="X"){
                 $(".referee").show().text("Player X wins");
+                
             }
       
         if($(".first-row").children().eq(i).val()=="X"&&$(".second-row").children().eq(i).val()=="X"&&$(".third-row").children().eq(i).val()=="X"){
             $(".referee").show().text("Player X wins");
+           
         }
         if($(".first-row").children().eq(0).val()=="X"&&$(".second-row").children().eq(1).val()=="X"&&$(".third-row").children().eq(2).val()=="X"){
             $(".referee").show().text("Player X wins");
+            
         }
         if($(".first-row").children().eq(2).val()=="X"&&$(".second-row").children().eq(1).val()=="X"&&$(".third-row").children().eq(0).val()=="X"){
             $(".referee").show().text("Player X wins");
+            
         }
        
     }
+    if($(".referee").text()=="Player X wins"){
+
+                
+        scoreX++;
+$(".result-X").text(scoreX);
+$("input[type='text']").attr("disabled",true);
+}
     })
     })
 })
@@ -90,52 +106,41 @@ $(document).ready(function(){
         for(let i=0 ;i<fieldContent.length;i++){
 
             if($(".field").eq(i).val()=="O"&&$(".field").eq(i).siblings().eq(0).val()=="O"&&$(".field").eq(i).siblings().eq(1).val()=="O"){
-                $(".referee").show().text("Player O wins");
+                $(".referee").show().text("Player O wins");   
             }
          
         if($(".first-row").children().eq(i).val()=="O"&&$(".second-row").children().eq(i).val()=="O"&&$(".third-row").children().eq(i).val()=="O"){
-            $(".referee").show().text("Player O wins");
+            $(".referee").show().text("Player O wins");  
         }
         if($(".first-row").children().eq(0).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(2).val()=="O"){
-            $(".referee").show().text("Player O wins");
+            $(".referee").show().text("Player O wins");   
         }
         
         if($(".first-row").children().eq(2).val()=="O"&&$(".second-row").children().eq(1).val()=="O"&&$(".third-row").children().eq(0).val()=="O"){
-            $(".referee").show().text("Player O wins");
+            $(".referee").show().text("Player O wins");   
         }
+    }
+    if($(".referee").text()=="Player O wins"){
+        scoreO++;
+        $(".result-O").text(scoreO);
+$("input[type='text']").attr("disabled",true);
     }
     })
     })
 })
 
 
-
-//calculate the score
-let scoreX=0;
-let scoreO=0;
-$(".result-O").text(scoreO);
-$(".result-X").text(scoreX);
-$(document).ready(function(){
-    $(function(){
-        $(".button").on("click",function(){
-            $("input[type='text']").val("").attr("disabled",false);
-            
-            
-                if($(".referee").text()=="Player O wins"){
-                    scoreO++;
-                    $(".result-O").text(scoreO);
-                    
-                
-                }
-                if($(".referee").text()=="Player X wins"){
-                    scoreX++;
-                    $(".result-X").text(scoreX);
-                
-                }
-                $(".referee").text("");
-               return index=0;
-
-        })
+    
+  //Again          
+  $(document).ready(function(){
+    $(".button").on("click",function(){
+        $("input[type='text']").val("").attr("disabled",false);
         
-    })
+    $(".referee").text("");
+   return index=0;
+
 })
+  })              
+                
+        
+   
